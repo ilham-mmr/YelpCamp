886e2521e0745a0aed6ee53f094ab3f52c84f435
Ilham MMR <ilham.mmr@gmail.com>
2021-03-01 23:40:18 +0700

    change the cluster map center

--- public/js/clusterMap.js
+++ public/js/clusterMap.js
@@ -2,7 +2,7 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
-    center: [-103.59179687498357, 40.66995747013945],
+    center: [108.2935527,9.25, -6.6622423],
     zoom: 3
 });
 
